5bd46b6bd3248646e3c8b6cd5203a63813f05c62
derciosinione <derciosinione@gmail.com>
2021-11-11 22:46:47 +0100

    Register the background service in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,9 @@ namespace SQSDemoBackgroundService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSingleton(_ => Configuration);
+            services.AddHostedService<SqsBackGroundService>();
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
