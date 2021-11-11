b6965573497a5f7275aa3266241ce4b2239077e1
derciosinione <derciosinione@gmail.com>
2021-11-11 23:04:33 +0100

    Added Method to Get the QueueUrl or Create a New Queue

--- SqsBackGroundService.cs
+++ SqsBackGroundService.cs
@@ -1,14 +1,47 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace SQSDemoBackgroundService
 {
     public class SqsBackGroundService : BackgroundService
     {
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        private readonly IConfiguration _configuration;
+
+        public SqsBackGroundService(IConfiguration configuration)
         {
-            throw new System.NotImplementedException();
+            _configuration = configuration;
+        }
+        
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var client = CreateClient();
+        }
+
+        private static async Task<string> GetQueueUrl(IAmazonSQS client, string queueName)
+        {
+            try
+            {
+                var response = await client.GetQueueUrlAsync(new GetQueueUrlRequest()
+                {
+                    QueueName = queueName
+                });
+
+                return response.QueueUrl;
+            }
+            catch (QueueDoesNotExistException)
+            {
+                var response = await client.CreateQueueAsync(new CreateQueueRequest()
+                {
+                    QueueName = queueName
+                });
+
+                return response.QueueUrl;
+            }
         }
     }
 }
