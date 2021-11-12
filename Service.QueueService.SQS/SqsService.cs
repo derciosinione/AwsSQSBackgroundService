31f86104f03cec0c9385e0a428af37a8d9caf774
derciosinione <derciosinione@gmail.com>
2021-11-12 17:14:49 +0100

    Added Service.QueueService Reference in Service.QueueService.Sqs project

--- Service.QueueService.SQS/SqsService.cs
+++ Service.QueueService.SQS/SqsService.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Service.QueueService.SQS
 {
-    public class SqsService
+    public class SqsService : IQueueService
     {
-        
+        public async Task<string> GetQueueUrlAsync(string queueName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> PublishToQueueAsync(string queueUrl, string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<QueueMessage>> ReceiveMessageAsync(string queueUrl, int maxMessages = 1)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task DeleteMessageAsync(string queueUrl, string id)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
