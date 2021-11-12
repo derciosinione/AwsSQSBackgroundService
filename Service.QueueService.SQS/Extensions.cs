8222445721c48fe7c37c5f940622a34e91245add
derciosinione <derciosinione@gmail.com>
2021-11-12 17:49:04 +0100

    Created Extention AddSqsService in Service.QueueService.Sqs project

--- Service.QueueService.SQS/Extensions.cs
+++ Service.QueueService.SQS/Extensions.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using Amazon.SQS;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Service.QueueService.SQS
 {
-    public class Extensions
+    public static class Extensions
     {
+        public static IServiceCollection AddSqsService(this IServiceCollection services)
+        {
+            services.AddAWSService<IAmazonSQS>();
+            services.AddScoped<IQueueService, SqsService>();
+
+            return services;
+        }
     }
 }
